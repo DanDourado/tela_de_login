6899e9221a6540795bc16ee2233c920cf254f255
DANIEL DOS SANTOS DOURADO <santosdaniel1927@gmail.com>
2023-12-21 00:32:23 -0300

    somente a tela de login

---
+++ script.js
@@ -0,0 +1,16 @@
+const database = [
+    {email: "teste1@gmail.com", password: "1123456781"},
+    {email: "teste2@gmail.com", password: "2123456782"},
+    {email: "teste3@gmail.com", password: "3123456783"},
+    {email: "teste4@gmail.com", password: "4123456784"},
+    {email: "teste5@gmail.com", password: "5123456785"},
+    {email: "teste6@gmail.com", password: "6123456786"},
+    {email: "teste7@gmail.com", password: "7123456787"},
+    {email: "teste8@gmail.com", password: "8123456788"},
+    {email: "teste9@gmail.com", password: "9123456789"}
+]
+const inputemail = document.getElementById("email")
+const inputsenha = document.getElementById("senha")
+const button = document.getElementById("button")
+
+button.addEventListener("click", )
